e740fbb2392320ebd7a8c032524e0daf9925e65e
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-12-01 13:53:26 +0700

    init

---
+++ react-smooth-scroll/src/views/Section2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Section2 = () => {
+  return (
+    <section
+      name="section2"
+      className="flex justify-center items-center h-screen w-full bg-gradient-to-r from-lime-400 to-green-500"
+    >
+      <div className="text-5xl">Section2</div>
+    </section>
+  );
+};
+
+export default Section2;
